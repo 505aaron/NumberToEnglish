02c53c7c12b761d98c9bc141d458baefca1176f6
Aaron Cordova <acordova05@gmail.com>
2017-09-16 09:00:10 -0600

    Improving the command line example

--- src/main/java/com/lessrework/text/CommandLineTester.java
+++ src/main/java/com/lessrework/text/CommandLineTester.java
@@ -2,6 +2,7 @@ package com.lessrework.text;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -9,23 +10,29 @@ import java.time.Instant;
 public class CommandLineTester {
 
     @Parameter(names = {"--value", "-v"}, description = "The value to format.", required = true)
-    private long value;
+    private Long value = null;
 
     @Parameter(names = {"--measure", "-m"}, description = "Measures format time")
-    private boolean measure;
+    private boolean measure = false;
 
     public static void main(String[] args) {
         CommandLineTester options = new CommandLineTester();
-        JCommander.newBuilder()
-            .addObject(options)
-            .build()
-            .parse(args);
+
+        try {
+            JCommander.newBuilder()
+                    .addObject(options)
+                    .build()
+                    .parse(args);
+        } catch (ParameterException e) {
+            e.usage();
+            return;
+        }
 
         Instant start = Instant.now();
         NumberToWordFormat format = new NumberToWordFormat();
         StringBuffer buffer = new StringBuffer();
         format.format(options.value, buffer, null);
-        System.out.printf("%d,'%s'%n", options.value, buffer.toString());
+        System.out.printf("%,d,'%s'%n", options.value, buffer.toString());
 
         Instant end = Instant.now();
 
